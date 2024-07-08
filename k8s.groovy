9cd2bc603d7fd4ca8f19da964f3747bd8545c8e2
Alexandr Aliferenko <set1991@tut.by>
2024-07-08 10:27:53 +0300

    changes

--- k8s.groovy
+++ k8s.groovy
@@ -26,6 +26,23 @@ pipeline {
                 sh "terraform apply -auto-approve"
             }
         }
+        stage('Ansible Run') {
+            steps {
+                script {
+                    // get EC2 IPs
+                    //def ips = sh(script: "aws ec2 describe-instances --filters 'Name=instance.group-name,Values=es_sg' --query 'Reservations[*].Instances[*].PublicIpAddress' --output text", returnStdout: true).trim().split()
+                    
+                    // create Ansible inventory file
+                    //writeFile file: 'hosts', text: "[elasticsearch]\n" + ips.join("\n")
+
+                    // start Ansible playbook
+                    cd ansible_project
+                    sh 'ansible-playbook playbook.yml'
+                }
+            }
+        }
+
+
         // stage ('destroy IaC') {
         //     agent { label 'master' }
         //     steps {
