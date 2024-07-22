cd939d71ea7909b0bc6bb7b555b19982d8850bc1
Alexandr Aliferenko <set1991@tut.by>
2024-07-22 12:03:30 +0300

    change backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
  backend "gcs" {
-   bucket  = "bucket-tfstate-aliferenko91"
-   credentials = "/home/set/keys/mygcp-cred.json"
+   bucket  = "bucket-tfstate-aliferenko1991"
+   credentials = "/home/set/keys/new-mygcp-cred.json"
    #credentials = var.gcp_srv_key
    prefix = "kubernetes/state"
 }
